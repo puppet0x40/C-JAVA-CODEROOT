cebf40c866641f82ff9323f65fefcc8d051cae9c
puppet0x40 <https://github.com/puppet0x40/TAD.git>
2019-05-05 17:21:38 -0300

    Create main.cpp

---
+++ BASICO/desligarpc C++/main.cpp
@@ -0,0 +1,12 @@
+#include "menu.hpp"
+int main(){
+    system("title Desligamento Programado");
+    mainMenu();
+    return 0;
+}
+
+/**
+proxima atualizacao: atualizar para interface grafica; win32/wx ou GTK
+
+*/
+
